cbed10ce916b2e46058cb32f4e7ec01ed980a0e0
Misha <misha.skopenko@yandex.ru>
2020-09-30 00:41:58 +0300

    another test commmit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div>
         <Container maxWidth="sm">
-          <h1>My Reasdfdsfdct App!</h1><Button variant="contained" color="primary">Hello World
+          <h1>My Reasdfdsfdct App!</h1>
+          <Button variant="contained" color="primary">
+            Hello World
           </Button>
         </Container>
       </div>
