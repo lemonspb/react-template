e25a3dd3833782dc70cc1194cf8e00dd3254ce8e
Misha <misha.skopenko@yandex.ru>
2020-09-30 00:35:38 +0300

    test commit

---
+++ src/components/app/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Button, Container } from "@material-ui/core";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Container maxWidth="sm">
+          <h1>My Reasdfdsfdct App!</h1>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
